69f90445c21d80a4f5855925339f945ed2fb734e
sulav kharel <kharel.sulav2016@gmail.com>
2019-08-05 15:42:55 +0900

    created the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+channels = names.map do |name|
+  Channel.find_or_create_by(name: name)
+end
+
+users = nicknames.map do |nickname|
+  User.create(email: "#{nickname.downcase}@me.com", nickname: nickname, password: "secret123")
+end
+
+20.times do
+  Message.create! user: user.sample, channel: channel.sample,content: Faker::HowIMetYourMother.quote
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}:: #{{channel.name}}"
+end
